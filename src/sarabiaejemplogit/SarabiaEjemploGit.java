b677ba15428c2b48431b35c8af77ca5f501d8d02
psh22 <psh22@LAPTOP-I5AV0SA6>
2022-04-28 18:07:33 +0200

    Version 1

---
+++ src/sarabiaejemplogit/SarabiaEjemploGit.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package sarabiaejemplogit;
+
+import java.io.*;
+/**
+ *
+ * @author psh22
+ */
+public class SarabiaEjemploGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Pablo Sarabia, version 1");
+    }
+
+}
